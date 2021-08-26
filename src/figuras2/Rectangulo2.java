d0efc58166c5d58a787010a2df92a2ec3f3cee20
Andres Trostdorf <87342125+TrostdorfA@users.noreply.github.com>
2021-08-26 13:58:49 -0300

    Update Rectangulo2.java

--- src/figuras2/Rectangulo2.java
+++ src/figuras2/Rectangulo2.java
@@ -14,8 +14,12 @@ public class Rectangulo2 {
 	public String toString() {
 		return "Rectangulo2 [ancho=" + ancho + ", alto=" + alto + "]";
 	}
-	
+
 	public double getPerimetro() {
 		return this.alto * 2 + this.ancho * 2;
+	}
+
+	public double getPerimetro() {
+		return this.alto * this.ancho;
 	}
 }
